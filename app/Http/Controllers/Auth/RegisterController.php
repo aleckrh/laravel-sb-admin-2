918d8856b2a38cc947289b847da3763cfeba4d4b
Alejandro RH <alejandro.rohu@gmail.com>
2020-11-17 09:45:23 -0500

    fix password change

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -69,7 +69,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'last_name' => $data['last_name'],
             'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+            'password' => $data['password'],
         ]);
     }
 }
