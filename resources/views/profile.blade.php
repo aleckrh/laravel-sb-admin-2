57f9db8c3dea32e1766138116dbf2df74fb9b9c9
Alejandro RH <alejandro.rohu@gmail.com>
2021-03-20 12:40:48 -0500

    fix password reset

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -4,6 +4,15 @@
     <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800">{{ __('Profile') }}</h1>
 
+    @if (session('success'))
+        <div class="alert alert-success border-left-success alert-dismissible fade show" role="alert">
+            {{ session('success') }}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    @endif
+
     @if ($errors->any())
         <div class="alert alert-danger border-left-danger" role="alert">
             <ul class="pl-4 my-2">
