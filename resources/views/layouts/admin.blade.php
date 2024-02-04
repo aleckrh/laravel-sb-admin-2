0517d85b881d6f6c020a9bef4c1837175660b15a
Alejandro RH <alejandro.rohu@gmail.com>
2024-02-03 23:03:16 -0500

    :fire: Update project

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -286,7 +286,7 @@
         <footer class="sticky-footer bg-white">
             <div class="container my-auto">
                 <div class="copyright text-center my-auto">
-                    <span>Copyright &copy; Alejandro RH {{ now()->year }}</span>
+                    <span>Copyright &copy; <a href="https://github.com/aleckrh" target="_blank">Aleckrh</a> {{ now()->year }}</span>
                 </div>
             </div>
         </footer>
